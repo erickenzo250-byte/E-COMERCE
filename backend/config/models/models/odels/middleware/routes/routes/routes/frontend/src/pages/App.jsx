750f37d51b5a9a7695b864549036ef2a33a0a3ef
erickenzo250-byte <erickenzo250@gmail.com>
2025-09-20 18:35:07 +0300

    Create App.jsx

---
+++ backend/config/models/models/odels/middleware/routes/routes/routes/frontend/src/pages/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import SellerDashboard from './pages/SellerDashboard';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/seller" element={<SellerDashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
