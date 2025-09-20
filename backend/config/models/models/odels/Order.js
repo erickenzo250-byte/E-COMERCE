6d6d71a469e616e69b70294a4f9de1e5354e9522
erickenzo250-byte <erickenzo250@gmail.com>
2025-09-20 18:30:05 +0300

    Create Order.js

---
+++ backend/config/models/models/odels/Order.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const OrderSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    products: [
+        {
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+            quantity: { type: Number, default: 1 }
+        }
+    ],
+    totalPrice: { type: Number },
+    createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Order', OrderSchema);
